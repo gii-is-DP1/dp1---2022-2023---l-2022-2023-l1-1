640688b0ee7b0e283d15171e0624a838678e2896
Alvaro Gonzalez <alvgonfri@alum.us.es>
2022-11-05 23:54:07 +0100

    Buscar partidas por nombre + Relación user-player

--- src/main/java/org/springframework/samples/petclinic/player/PlayerController.java
+++ src/main/java/org/springframework/samples/petclinic/player/PlayerController.java
@@ -1,7 +1,9 @@
 package org.springframework.samples.petclinic.player;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class PlayerController {
     private PlayerService playerService;
 
