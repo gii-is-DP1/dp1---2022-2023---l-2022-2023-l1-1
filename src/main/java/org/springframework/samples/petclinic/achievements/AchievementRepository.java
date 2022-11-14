f53d67fcccac9b83c867fb45056aec06c6062a2c
Miguel Manzano Álvarez <73229701+miguelma1@users.noreply.github.com>
2022-11-14 18:57:06 +0100

    Modificacion achievementController y  Progress

--- src/main/java/org/springframework/samples/petclinic/achievements/AchievementRepository.java
+++ src/main/java/org/springframework/samples/petclinic/achievements/AchievementRepository.java
@@ -2,14 +2,11 @@ package org.springframework.samples.petclinic.achievements;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AchievementRepository extends CrudRepository<Achievement, Integer>{
     List<Achievement> findAll();
-    
     
 }
