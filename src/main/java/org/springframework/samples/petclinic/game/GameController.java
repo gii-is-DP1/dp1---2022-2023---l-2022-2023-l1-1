5a2e5e35cffd1eccb9f4b11160d01e9eddd8bb9e
davgonher1 <72940888+gahijil@users.noreply.github.com>
2022-11-16 11:01:02 +0100

    Eliminación del comentario prueba github

--- src/main/java/org/springframework/samples/petclinic/game/GameController.java
+++ src/main/java/org/springframework/samples/petclinic/game/GameController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-//PRUEBA GITHUB1
+
 @Controller
 @RequestMapping("/games")
 public class GameController {
