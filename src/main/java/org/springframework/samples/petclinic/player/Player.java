e139b7b2941ac9ab63cc6973b2c8cef3fcd444c8
Alvaro Gonzalez <alvgonfri@alum.us.es>
2022-12-15 03:50:03 +0100

    Small changes

--- src/main/java/org/springframework/samples/petclinic/player/Player.java
+++ src/main/java/org/springframework/samples/petclinic/player/Player.java
@@ -1,12 +1,17 @@
 package org.springframework.samples.petclinic.player;
 
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import org.springframework.samples.petclinic.deck.Deck;
 import org.springframework.samples.petclinic.model.BaseEntity;
+import org.springframework.samples.petclinic.progress.Progress;
 import org.springframework.samples.petclinic.user.User;
 
 import lombok.Getter;
@@ -25,4 +30,10 @@ public class Player extends BaseEntity {
     @OneToOne
     @JoinColumn(name = "username", referencedColumnName = "username")
 	private User user;
+
+    @OneToMany (mappedBy = "player")
+    private List<Deck> decks;
+
+    @OneToMany (mappedBy = "player")
+    private List<Progress> progress;
 }
