b06283500bb07150fe950f8772f0e3ec46ddf0cd
Miguel Manzano Álvarez <73229701+miguelma1@users.noreply.github.com>
2023-01-07 23:41:26 +0100

    Test de turnService

--- src/test/java/org/springframework/samples/petclinic/turn/TurnServiceTest.java
+++ src/test/java/org/springframework/samples/petclinic/turn/TurnServiceTest.java
@@ -3,11 +3,16 @@ package org.springframework.samples.petclinic.turn;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.springframework.samples.petclinic.deck.VoteCard.VCType;
 import org.springframework.samples.petclinic.enums.CurrentRound;
 import org.springframework.samples.petclinic.enums.CurrentStage;
@@ -17,6 +22,7 @@ import org.springframework.samples.petclinic.game.GameRepository;
 import org.springframework.samples.petclinic.game.GameService;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 public class TurnServiceTest {
 
     @Mock
@@ -24,6 +30,13 @@ public class TurnServiceTest {
 
     @Mock
     GameRepository gameRepository;
+
+    Turn turn;
+    Game game;
+    Integer originalLoyalVotes;
+    Integer originalTraitorVotes;
+    Integer originalNeutralVotes;
+    Integer originalTurn;
 
     private Turn createTurn(Integer currentTurn, Integer votesLoyal, Integer votesTraitor, Integer votesNeutral) {
         Turn turn = new Turn();
@@ -34,21 +47,33 @@ public class TurnServiceTest {
         return turn;
     }
 
-    private Game createGame(String name, Boolean publicGame, Turn turn, Integer numPlayers) {
+    private Game createGame(String name, Boolean publicGame) {
         Game game = new Game();
         game.setName(name);
         game.setPublicGame(publicGame);
         game.setState(State.IN_PROCESS);
-        game.setNumPlayers(numPlayers);
+        game.setNumPlayers(5);
         game.setRound(CurrentRound.FIRST);
         game.setTurn(turn);
         return game;
     }
 
+    @BeforeEach
+    public void config() {
+        turn = createTurn(4, 0, 0, 0);
+        game = createGame("Testing game", true);
+        game.setTurn(turn);
+        originalTurn = turn.getCurrentTurn();
+        originalLoyalVotes = turn.getVotesLoyal();
+        originalTraitorVotes = turn.getVotesTraitor();
+        originalNeutralVotes = turn.getVotesNeutral();
+
+        when(turnRepository.findGameByTurn(any(Turn.class))).thenReturn(game);
+    }
+
     @Test
-    public void testSaveGameSuccessful() {
-        Turn turn = createTurn(1, 0, 0, 0);
-        TurnService turnService = new TurnService(turnRepository);
+    public void testSaveTurnSuccessful() {
+        TurnService turnService = new TurnService(turnRepository, gameRepository);
         try {
             turnService.save(turn);
         } catch (Exception e) {
@@ -58,11 +83,8 @@ public class TurnServiceTest {
 
     @Test
     public void testUpdateTurnVotes () { //funciona
-        Turn turn = createTurn(1, 0, 0, 0);
-        Integer originalLoyalVotes = turn.getVotesLoyal();
-        Integer originalTraitorVotes = turn.getVotesTraitor();
-        Integer originalNeutralVotes = turn.getVotesNeutral();
-        TurnService turnService = new TurnService(turnRepository);
+        
+        TurnService turnService = new TurnService(turnRepository, gameRepository);
 
         turnService.updateTurnVotes(turn, VCType.YELLOW);
         assertTrue(turn.getVotesLoyal() == originalLoyalVotes);
@@ -82,22 +104,20 @@ public class TurnServiceTest {
 
     @Test
     public void testNewTurn() { //no sale
-        Turn turn = createTurn(3, 0, 0, 0);
-
-        Game game = createGame("Testing new turn", true, turn, 5);
-
-        TurnService turnService = new TurnService(turnRepository);
+        TurnService turnService = new TurnService(turnRepository, gameRepository);
         turnService.newTurn(turn);
+        assertTrue(turn.getCurrentTurn() == originalTurn + 1);
+        assertTrue(game.getRound() == CurrentRound.FIRST);
+        turnService.newTurn(turn);
+        assertTrue(turn.getCurrentTurn() == 1);
+        assertTrue(game.getRound() == CurrentRound.SECOND);
 
     }
 
     @Test
     public void testPretorVoteChange() { //funciona
-        Turn turn = createTurn(1, 1, 1, 0);
-        Game game = createGame("Testing new turn", true, turn, 5);
-        Integer originalLoyalVotes = turn.getVotesLoyal();
-        Integer originalTraitorVotes = turn.getVotesTraitor();
-        TurnService turnService = new TurnService(turnRepository);
+        
+        TurnService turnService = new TurnService(turnRepository, gameRepository);
 
         turnService.pretorVoteChange(VCType.GREEN, VCType.GREEN, game);
         assertTrue(originalLoyalVotes == game.getTurn().getVotesLoyal());
@@ -111,8 +131,9 @@ public class TurnServiceTest {
         assertTrue(originalLoyalVotes == game.getTurn().getVotesLoyal() - 1);
         assertTrue(originalTraitorVotes == game.getTurn().getVotesTraitor() + 1);
 
-        turn = createTurn(1, 1, 1, 0);
-        game = createGame("Testing new turn", true, turn, 5);
+        turn = createTurn(3, 0, 0, 0);
+        game = createGame("Testing game", true);
+        game.setTurn(turn);
 
         turnService.pretorVoteChange(VCType.GREEN, VCType.RED, game);
         assertTrue(originalLoyalVotes == game.getTurn().getVotesLoyal() + 1);
