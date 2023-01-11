3d97b560775db0ea49f8c9ab3e6e99ab0a79835a
Alvaro Gonzalez <alvgonfri@alum.us.es>
2023-01-11 23:48:48 +0100

    New repository tests

--- src/main/java/org/springframework/samples/petclinic/user/UserRepository.java
+++ src/main/java/org/springframework/samples/petclinic/user/UserRepository.java
@@ -9,12 +9,9 @@ import org.springframework.data.repository.CrudRepository;
 public interface UserRepository extends  CrudRepository<User, String>{
     List<User> findAll();
 
-    @Query("SELECT u FROM User u ")
-	public List<User> findUsers();
-
 	@Query("SELECT u.username FROM User u ")
 	public List<String> findUsernames();
 
-	@Query("SELECT a.user FROM Authorities a WHERE a.authority = ?1 ")
+	@Query("SELECT a.user FROM Authorities a WHERE a.authority = ?1")
 	public List<User> findUserWithAuthority(String authority);
 }
